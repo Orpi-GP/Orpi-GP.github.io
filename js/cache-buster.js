608d67feae4ecc1aa92c577c95c466e8b2d970c3
Orpi-GP <86384742+Orpi-GP@users.noreply.github.com>
2025-10-26 20:13:33 +0100

    Update cache-buster.js

--- js/cache-buster.js
+++ js/cache-buster.js
@@ -3,8 +3,19 @@
     stylesheets.forEach(link => {
         const href = link.getAttribute('href');
         if (href && href.includes('css/styles.css')) {
-            const separator = href.includes('?') ? '&' : '?';
-            link.setAttribute('href', href.split('?')[0] + '?v=' + new Date().getTime());
+            const newHref = href.split('?')[0] + '?v=' + new Date().getTime();
+            
+            const preloadLink = document.createElement('link');
+            preloadLink.rel = 'preload';
+            preloadLink.as = 'style';
+            preloadLink.href = newHref;
+            
+            preloadLink.onload = function() {
+                link.href = newHref;
+                setTimeout(() => preloadLink.remove(), 100);
+            };
+            
+            document.head.appendChild(preloadLink);
         }
     });
 })();
